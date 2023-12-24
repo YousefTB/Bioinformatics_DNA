dda209656324f3889d65d16f8dccc93e98c53bc1
Yousef360 <95775013+YousefTB@users.noreply.github.com>
2023-12-24 21:28:20 +0200

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,14 +1,15 @@
 from bioinformatics import *
 import streamlit as st
-cnt = 0
-st.title("Bioinformatics Application - BFCAI\n")
-st.header("An introduction\n")
+
+st.image('bioinformatics.jpg', use_column_width=True)
+st.title(":blue[Bioinformatics Application - BFCAI]")
+st.header("Welcome To Our Website")
 st.markdown("**This application for educational purposes only**")
 st.markdown("""**The biology field is intersected with many of `AI` applications, which is important to use to identify
 the sequences of DNA, the translation into protein sequence and searching for `PATTERNS` !**
 """)
 st.divider()
-tab1, tab2 = st.tabs(['DNA Translation', 'DNA Sequence Searching'])
+tab1, tab2, tab3 = st.tabs(['DNA Translation', 'DNA Sequence Searching', 'Approximate Matching'])
 with tab1:
     file = st.file_uploader("Fasta file", type=['fasta','fa'], key=0)
     text = None
@@ -191,10 +192,53 @@ with tab2:
                     show_text.write(found_pattern)
             else:
                 show_text.write("The pattern is not found")
-                
     else:
-        st.write("No DNA sequence is added yet !")
+        st.write(":red[No DNA sequence is added yet !]")
+with tab3:
+    file = st.file_uploader("Fasta file", type=['fasta','fa'],key=2)
+    text = None
+    if file is not None:
+        text = file.getvalue().decode(encoding='utf-8')
+        text = text.split('\n')[1]
 
+    st.subheader("This is your DNA sequence:")
+    if text is not None:
+        selected = -1
+        st.write(text)
+        pattern2 = st.text_input("**Pattern required**", placeholder="Enter your pattern to search in UPPERCASE", value=None)
+        if 'prev_t2' not in st.session_state:
+                st.session_state.prev_t2 = None
+        if 'cnt2' not in st.session_state:
+                    st.session_state.cnt2 = 0
+        if st.session_state.prev_t2 != pattern2:
+            st.session_state.cnt2 = 0
+        
+        st.session_state.prev_t2 = pattern2
 
-    
+        if pattern2 != None:
+            found, array = approximate_matching(text,pattern2)
+            new_text, new_pattern, edits, percentage, ix = interpret_solution_approximate(found, array, 
+                                       st.session_state.cnt2, text, pattern2)
+            show_index = st.empty()
+            show_index.write(f"**Found at index {ix}**")
+            st.write("**`Only part of the text will be shown where the pattern is found`**")
+            show_text = st.empty()
+            show_text.write(new_text + '\n\n' + new_pattern)
 
+            show_per = st.empty()
+            show_edits = st.empty()
+            show_per.write(f':green[**Matching Percentage %:** ] `{percentage * 100}`')
+            show_edits.write(f':red[**Num. of edits performed:** ] {edits}')
+            if st.button('Next location', key=3):
+                st.session_state.cnt2 += 1
+                if st.session_state.cnt2 >= len(found):
+                    st.session_state.cnt2 = 0
+                new_text, new_pattern, edits, percentage, ix = interpret_solution_approximate(found, array, 
+                                       st.session_state.cnt2, text, pattern2)
+                show_index.write(f"**Found at index {ix}**")
+                show_text.write(new_text + '\n\n' + new_pattern)
+                show_per.write(f':green[**Matching Percentage %:** ] `{percentage * 100}`')
+                show_edits.write(f':red[**Num. of edits performed:** ] {edits}')
+        
+    else:
+        st.write(":red[No DNA sequence is added yet !]")
