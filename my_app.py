1d7545c63f01a18f635dc27c33a3a3ca25bc6103
Yousef360 <lover.avenger.9@gmail.com>
2023-12-24 03:39:40 +0200

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -3,5 +3,7 @@ import streamlit as st
 
 st.title("Bioinformatics Application - BFCAI\n")
 st.header("An introduction\n")
-st.markdown("**This application for educational purposes\n**")
-st.markdown("**The biology field is intersected with many of `AI` applications, which is important to use to identify**")
+st.markdown("**This application for educational purposes only**")
+st.markdown("""**The biology field is intersected with many of `AI` applications, which is important to use to identify
+the sequences of DNA and much more**
+""")
