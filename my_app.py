5c4ad43ba47d6dcdafb1078443f810c8a4d591de
Yousef360 <lover.avenger.9@gmail.com>
2023-12-24 03:43:53 +0200

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -5,5 +5,6 @@ st.title("Bioinformatics Application - BFCAI\n")
 st.header("An introduction\n")
 st.markdown("**This application for educational purposes only**")
 st.markdown("""**The biology field is intersected with many of `AI` applications, which is important to use to identify
-the sequences of DNA and much more**
+the sequences of DNA, the translation into protein sequence and searching for `PATTERNS` !**
 """)
+st.divider()
